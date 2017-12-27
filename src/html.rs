d4e35ce555fe8c69fa1df570cc61cda8afc1e91c
jD91mZM2 <me@krake.one>
2017-12-27 13:12:53 +0100

    Patch HTML

--- src/html.rs
+++ src/html.rs
@@ -18,6 +18,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+// This is a copy of pulldown_cmark's html.rs, modified to work in GTK.
+
 //! HTML renderer that takes an iterator of events as input.
 
 use std::borrow::Cow;
@@ -75,79 +77,17 @@ impl<'a, 'b, I: Iterator<Item=Event<'a>>> Ctx<'b, I> {
 
     fn start_tag(&mut self, tag: Tag<'a>, numbers: &mut HashMap<Cow<'a, str>, usize>) {
         match tag {
-            Tag::Paragraph =>  {
-                self.fresh_line();
-                self.buf.push_str("<p>");
-            }
-            Tag::Rule => {
-                self.fresh_line();
-                self.buf.push_str("<hr />\n")
-            }
             Tag::Header(level) => {
                 self.fresh_line();
-                self.buf.push_str("<h");
-                self.buf.push((b'0' + level as u8) as char);
-                self.buf.push('>');
-            }
-            Tag::Table(alignments) => {
-                self.table_alignments = alignments;
-                self.buf.push_str("<table>");
-            }
-            Tag::TableHead => {
-                self.table_state = TableState::Head;
-                self.buf.push_str("<thead><tr>");
-            }
-            Tag::TableRow => {
-                self.table_cell_index = 0;
-                self.buf.push_str("<tr>");
-            }
-            Tag::TableCell => {
-                match self.table_state {
-                    TableState::Head => self.buf.push_str("<th"),
-                    TableState::Body => self.buf.push_str("<td"),
-                }
-                match self.table_alignments.get(self.table_cell_index) {
-                    Some(&Alignment::Left) => self.buf.push_str(" align=\"left\""),
-                    Some(&Alignment::Center) => self.buf.push_str(" align=\"center\""),
-                    Some(&Alignment::Right) => self.buf.push_str(" align=\"right\""),
-                    _ => (),
-                }
-                self.buf.push_str(">");
-            }
-            Tag::BlockQuote => {
-                self.fresh_line();
-                self.buf.push_str("<blockquote>\n");
+                self.buf.push_str("<big>");
             }
             Tag::CodeBlock(info) => {
                 self.fresh_line();
-                let lang = info.split(' ').next().unwrap();
-                if lang.is_empty() {
-                    self.buf.push_str("<pre><code>");
-                } else {
-                    self.buf.push_str("<pre><code class=\"language-");
-                    escape_html(self.buf, lang, false);
-                    self.buf.push_str("\">");
-                }
+                self.buf.push_str("<tt>");
             }
-            Tag::List(Some(1)) => {
-                self.fresh_line();
-                self.buf.push_str("<ol>\n");
-            }
-            Tag::List(Some(start)) => {
-                self.fresh_line();
-                let _ = write!(self.buf, "<ol start=\"{}\">\n", start);
-            }
-            Tag::List(None) => {
-                self.fresh_line();
-                self.buf.push_str("<ul>\n");
-            }
-            Tag::Item => {
-                self.fresh_line();
-                self.buf.push_str("<li>");
-            }
-            Tag::Emphasis => self.buf.push_str("<em>"),
-            Tag::Strong => self.buf.push_str("<strong>"),
-            Tag::Code => self.buf.push_str("<code>"),
+            Tag::Emphasis => self.buf.push_str("<i>"),
+            Tag::Strong => self.buf.push_str("<b>"),
+            Tag::Code => self.buf.push_str("<tt>"),
             Tag::Link(dest, title) => {
                 self.buf.push_str("<a href=\"");
                 escape_href(self.buf, &dest);
@@ -157,67 +97,19 @@ impl<'a, 'b, I: Iterator<Item=Event<'a>>> Ctx<'b, I> {
                 }
                 self.buf.push_str("\">");
             }
-            Tag::Image(dest, title) => {
-                self.buf.push_str("<img src=\"");
-                escape_href(self.buf, &dest);
-                self.buf.push_str("\" alt=\"");
-                self.raw_text(numbers);
-                if !title.is_empty() {
-                    self.buf.push_str("\" title=\"");
-                    escape_html(self.buf, &title, false);
-                }
-                self.buf.push_str("\" />")
-            }
-            Tag::FootnoteDefinition(name) => {
-                self.fresh_line();
-                let len = numbers.len() + 1;
-                self.buf.push_str("<div class=\"footnote-definition\" id=\"");
-                escape_html(self.buf, &*name, false);
-                self.buf.push_str("\"><sup class=\"footnote-definition-label\">");
-                let number = numbers.entry(name).or_insert(len);
-                self.buf.push_str(&*format!("{}", number));
-                self.buf.push_str("</sup>");
-            }
+            _ => ()
         }
     }
 
     fn end_tag(&mut self, tag: Tag) {
         match tag {
-            Tag::Paragraph => self.buf.push_str("</p>\n"),
-            Tag::Rule => (),
-            Tag::Header(level) => {
-                self.buf.push_str("</h");
-                self.buf.push((b'0' + level as u8) as char);
-                self.buf.push_str(">\n");
-            }
-            Tag::Table(_) => {
-                self.buf.push_str("</tbody></table>\n");
-            }
-            Tag::TableHead => {
-                self.buf.push_str("</tr></thead><tbody>\n");
-                self.table_state = TableState::Body;
-            }
-            Tag::TableRow => {
-                self.buf.push_str("</tr>\n");
-            }
-            Tag::TableCell => {
-                match self.table_state {
-                    TableState::Head => self.buf.push_str("</th>"),
-                    TableState::Body => self.buf.push_str("</td>"),
-                }
-                self.table_cell_index += 1;
-            }
-            Tag::BlockQuote => self.buf.push_str("</blockquote>\n"),
-            Tag::CodeBlock(_) => self.buf.push_str("</code></pre>\n"),
-            Tag::List(Some(_)) => self.buf.push_str("</ol>\n"),
-            Tag::List(None) => self.buf.push_str("</ul>\n"),
-            Tag::Item => self.buf.push_str("</li>\n"),
-            Tag::Emphasis => self.buf.push_str("</em>"),
-            Tag::Strong => self.buf.push_str("</strong>"),
-            Tag::Code => self.buf.push_str("</code>"),
+            Tag::Header(level) => self.buf.push_str("</big>"),
+            Tag::CodeBlock(_) => self.buf.push_str("</tt>\n"),
+            Tag::Emphasis => self.buf.push_str("</i>"),
+            Tag::Strong => self.buf.push_str("</b>"),
+            Tag::Code => self.buf.push_str("</tt>"),
             Tag::Link(_, _) => self.buf.push_str("</a>"),
-            Tag::Image(_, _) => (), // shouldn't happen, handled in start
-            Tag::FootnoteDefinition(_) => self.buf.push_str("</div>\n"),
+            _ => ()
         }
     }
 
